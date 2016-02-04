d5e69f38e9b0adc558f858994149a5e2c59f6f6a
Evan Mitchell <emitchell@insightsquared.com>
2016-02-04 14:45:30 -0500

    Changes from findings post-El Capitan upgrade.

--- Preferences.el
+++ Preferences.el
@@ -4,15 +4,23 @@
 ;; evaluating this file and print errors in the *Messags* buffer.
 ;; Use this file in place of ~/.emacs (which is loaded as well.)
 
+;; Always start Aquamacs where C-x C-f opens new files in the 
+;; same buffer. 
+(one-buffer-one-frame-mode 0)
+
+;; Map OSX home and end key to behave like you expect.
 (define-key osx-key-mode-map [home] 'beginning-of-line)
 (define-key osx-key-mode-map [end] 'end-of-line)
 
-; Aquamacs Emacs Starter Kit
+;; Aquamacs Emacs Starter Kit
 ;; http://github.com/walter/aquamacs-emacs-starter-kit
 ;; set the dotfiles-dir variable to this directory
-;;(setq kitfiles-dir (concat (file-name-directory
-; ;                   (or (buffer-file-name) load-file-name)) "/aquamacs-emacs-\
-starter-kit"))
+;; (setq kitfiles-dir (concat (file-name-directory
+;;                     (or (buffer-file-name) load-file-name)) "/aquamacs-emacs-starter-kit"))
+
+;; set up our various directories to load
+;; (add-to-list 'load-path kitfiles-dir)
+;; (require 'init)
 
 ;; set up our various directories to load
 ;;(add-to-list 'load-path kitfiles-dir)
